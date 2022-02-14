c3d65850cdb0f2ead33b8c1d0f3908fad37747fd
apdkeb <apdkeb@naver.com>
2022-02-14 16:34:04 +0900

    feat : 비로그인 유효성 추가
    2022. 02. 14
    AuthenticationPrincipalArgumentResolver
    - 비로그인이면 401 에러 발생하게 유효성 추가
    
    FavoriteAcceptanceTest
    - 통과하는 비로그인 테스트 추가
    
    Resolver : loop-study
    See also : heesunjeong

--- src/test/java/nextstep/subway/acceptance/FavoriteSteps.java
+++ src/test/java/nextstep/subway/acceptance/FavoriteSteps.java
@@ -22,6 +22,18 @@ public class FavoriteSteps {
                 .then().log().all().extract();
     }
 
+    public static ExtractableResponse<Response> 비로그인_즐겨찾기_생성_요청(Long source, Long target) {
+        Map<String, Long> params = new HashMap<>();
+        params.put("source", source);
+        params.put("target", target);
+        return RestAssured
+                .given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/favorites")
+                .then().log().all().extract();
+    }
+
     public static ExtractableResponse<Response> 즐겨찾기_목록_조회_요청(String accessToken) {
         return RestAssured
                 .given().log().all()
@@ -30,11 +42,11 @@ public class FavoriteSteps {
                 .then().log().all().extract();
     }
 
-    public static ExtractableResponse<Response> 즐겨찾기_제거_요청(String accessToken, Long favoriteId) {
+    public static ExtractableResponse<Response> 즐겨찾기_제거_요청(String accessToken, String url) {
         return RestAssured
                 .given().log().all()
                 .auth().oauth2(accessToken)
-                .when().delete("/favorites/{lineId}", favoriteId)
+                .when().delete(url)
                 .then().log().all().extract();
     }
 }
